426e87eb9a83ead16c1850d89fe85bc6fd255790
Viviansol <viviansan93@gmail.com>
2022-11-09 20:04:34 -0300

    added login negative flow test

--- src/test/java/LoginPageTest.java
+++ src/test/java/LoginPageTest.java
@@ -7,27 +7,20 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class LoginPageTest {
     private static LoginPage page;
 
-    @BeforeEach
-    public void SetUp(){
-        page = new LoginPage();
-        page.openApp();
-
-    }
 
     @Test
-    public void loginFlowTest() throws InterruptedException{
+    public void LoginNegativeFlowTest() throws InterruptedException{
         page = new LoginPage();
         page.openApp();
         page.iniciarSessao();
-        page.insertLoginData("viviansan93@gmail", "123");
+        page.insertLoginData("viviansan93@gmail.com", "023456");
         Thread.sleep(2000);
         page.Login();
         Thread.sleep(2000);
         String messageErrorTest = page.getMessageLogin();
-
-
         assertTrue(messageErrorTest.contains("Sus credenciales son inválidas. Por favor, vuelva a intentarlo"));
 
-
     }
+
+
 }
